dfae0fed72513dad191ef893233fbe173ab22c1a
Amereth <alek.melnyk@gmail.com>
2024-02-10 01:19:05 +0200

    refactoring: created separate folder for header related components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
-import { Header } from '@/components/Header'
+import { Header } from '@/components/header'
 import { Box, Stack } from '@chakra-ui/react'
 import { Footer } from '@/components/Footer'
 import '../globals.css'
