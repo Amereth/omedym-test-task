bc834018bf23fde40a1cc4405709f0ac1567ecbb
Amereth <alek.melnyk@gmail.com>
2024-02-10 00:44:44 +0200

    feature: added link to task creation page in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
-import { Box, Center } from '@chakra-ui/react'
+import { Box, IconButton, Link, Tooltip } from '@chakra-ui/react'
 import Image from 'next/image'
 import { HeaderAuth } from './HeaderAuth'
+import { PlusIcon } from 'lucide-react'
 
 export const Header = () => {
   return (
-    <Center
+    <Box
+      display='flex'
+      alignItems='center'
       as='header'
       px={[2, 4, 8]}
       py={[2, 2, 4]}
       borderBottom='1px'
       borderColor='gray.700'
-      justifyContent='space-between'
     >
       <Box pos='relative' width={['100px', '200px']} height={['40px', '60px']}>
         <Image
@@ -20,7 +22,20 @@ export const Header = () => {
         />
       </Box>
 
+      <Tooltip label='Create new task'>
+        <Link href='create-task' ml='auto' mr={8}>
+          <IconButton
+            aria-label='Create new task'
+            icon={<PlusIcon />}
+            variant='outline'
+            size='md'
+          >
+            <PlusIcon />
+          </IconButton>
+        </Link>
+      </Tooltip>
+
       <HeaderAuth />
-    </Center>
+    </Box>
   )
 }
